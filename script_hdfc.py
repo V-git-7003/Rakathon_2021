f894b8cd729651e36243389318b571af87cd5fb3
Vasanth <bs.vasanth@gmail.com>
2021-02-10 04:05:23 +0530

    commit with first page

--- script_hdfc.py
+++ script_hdfc.py
@@ -1,16 +1,31 @@
 import scrapy
-
+from scrapy import Request
 class BrickSetSpider(scrapy.Spider):
     name = "brickset_spider"
     start_urls = ['https://www.moneycontrol.com/company-article/hdfcbank/news/HDF01']
-
+    
     def parse(self, response):
+        xp = 'div.pages a::attr(href)'
+
+        temp_list = []
+
+        for a_class in response.css(xp).extract():
+            temp_list.append("https://www.moneycontrol.com"+a_class)
+        
+        #st_url = 'https://www.moneycontrol.com/company-article/hdfcbank/news/HDF01'
+        temp_list.append(start_urls)
+        
+        print("temp list ",temp_list,'Length of the list',len(temp_list))
+        
+        return (Request(url, callback=self.parse_manga_list_page) for url in temp_list)
+       
+    
+    def parse_manga_list_page(self, response):
         SET_SELECTOR = 'div.MT15'
         for div_classes in response.css(SET_SELECTOR):
-         
               NAME_SELECTOR = 'a::attr(href)'
-              yield {'name':div_classes.css(NAME_SELECTOR).extract_first()
-              }
+              print('name', div_classes.css(NAME_SELECTOR).extract_first())
+              print()
+               
 
 
- #response.css('div.MT15 a::attr(href)').extract_first()      
