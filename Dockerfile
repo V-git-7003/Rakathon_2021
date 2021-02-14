a07d3141bf1ddb2c9c5b47fa8f3dbc6d98235f38
Krishna Sharma <krishnasharma@MacBook-Pro.local>
2021-02-14 21:35:40 +0530

    updated the code

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM python:3.8
 
-WORKDIR /hackathon_2021
+WORKDIR /Hackathon_2021
 
 RUN pip install scrapy
 
+RUN pip install boto3
+
 COPY / .
 
-CMD [ "python", "./src/test.py" ]
+CMD [ "python", "./src/bankingcrawler.py" ]
---
+++ src/test.py
@@ -0,0 +1,31 @@
+import logging
+import boto3
+import os,fnmatch
+from botocore.exceptions import ClientError
+
+
+def upload_file(bucket):
+    cwd = os.getcwd()
+    path_of_dir = format(cwd)
+    get_all_file = find("*.json",path_of_dir.replace("/src","/files"))
+    s3_client = boto3.client('s3',
+        aws_access_key_id="AKIAVM25SKAHN4NMSC2H",
+        aws_secret_access_key="st9FucT2XGsGaL90/fFluN7TCacgWcLheBS9A0ki"
+    )
+    try:
+        for file_name in get_all_file:
+            length = len(file_name.split("/"))
+            object_name = str(file_name.split("/")[length-1])
+            response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+def find(pattern, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(os.path.join(root, name))
+    return result
